50cdba4029e33d8634d17a030a535db07875b786
AnaisT17 <tchicayanais@gmail.com>
2019-09-19 17:24:15 +0200

    create variables on file script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict'
+
+const input = document.getElementById('userinput');
+const button = document.getElementById('enter');
+const ul = document.querySelector ('ul');
+const li = document.querySelector('li');
