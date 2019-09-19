c69394b9f3cda18367298a722dd8f393c410b725
AnaisT17 <tchicayanais@gmail.com>
2019-09-19 17:47:20 +0200

    add function input on file script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const input = document.getElementById('userinput');
 const button = document.getElementById('enter');
 const ul = document.querySelector ('ul');
 const li = document.createElement('li');
+
+function inputlenght () {
+    return input.value= "";
+}
+
+function addListAfterClick () {
+    if (inputlenght () > 0){
+        createElement ();
+    }
+}
+
